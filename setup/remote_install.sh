30a44ff66701c6bf3d5ea34e5d1c14665ac75449
Jaehyun Han <lovelyhyun@gmail.com>
2015-02-27 13:33:13 +0000

    Add remote install script

--- setup/remote_install.sh
+++ setup/remote_install.sh
@@ -4,7 +4,28 @@
 # For ubuntu 14.04 64bit
 # 2015.02.27 Jaehyun Han <jhhan@dcslab.snu.ac.kr>
 
-# set hosts
-cat hosts | ssh webserver "sudo sh -c 'cat >> /etc/hosts'"
-cat hosts | ssh appserver "sudo sh -c 'cat >> /etc/hosts'"
+# get hosts
+webserver=$(grep webserver hosts | awk '{print $1}')
+dbserver=$(grep dbserver hosts | awk '{print $1}')
+appserver=$(grep appserver hosts | awk '{print $1}')
+
+# set hosts and common
+cat hosts | ssh $webserver "sudo sh -c 'cat >> /etc/hosts'"
+ssh $webserver 'bash -s' < setup_common.sh
+
+cat hosts | ssh $dbserver "sudo sh -c 'cat >> /etc/hosts'"
+ssh $dbserver 'bash -s' < setup_common.sh
+
 cat hosts | ssh dbserver "sudo sh -c 'cat >> /etc/hosts'"
+ssh $appserver 'bash -s' < setup_common.sh
+
+# setup dbserver
+ssh $dbserver 'bash -s' < setup_db.sh
+
+# setup appserver
+ssh $appserver 'bash -s' < setup_was.sh
+
+# setup webserver
+ssh $webserver 'bash -s' < setup_web.sh
+
+
